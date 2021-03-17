6948a43741fce8d1e783f7a586623ab5d5dba7a5
TanchikiPro <tanchikipro7777777@gmail.com>
2021-03-17 07:59:19 +0300

    init repo

---
+++ TxtToExcelConverter_Tim/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TxtToExcelConverter_Tim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            
+            openFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            string fileName = openFileDialog.FileName;
+
+            richTextBox1.Text = File.ReadAllText(fileName, Encoding.GetEncoding(1251));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ TxtToExcelConverter_Tim/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TxtToExcelConverter_Tim
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
